e6fc390bd49b6ee595a4d1baaaaf1c68cd7c85e9
Prachi. Patel <prachi0patel0000@gmail.com>
2024-11-08 17:03:43 +0530

    Add project files.

---
+++ BookShoppingCartMvcUi/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingCartMvcUi.Models
+{
+    [Table("Author")]
+    public class Author
+    {
+        public int Id { get; set; }
+        public String AuthorName { get; set; }
+        public String Image { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public List<Book> Book { get; set; }
+    }
+}
---
+++ BookShoppingCartMvcUi/Repositories/UserOrderRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace BookShoppingCartMvcUi.Repositories
+{
+    public class UserOrderRepository :IUserOrderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserOrderRepository(ApplicationDbContext db, IHttpContextAccessor httpContextAccessor, UserManager<IdentityUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task ChangeOrderStatus(UpdateOrderStatusModel data)
+        {
+            var order = await _db.orders.FindAsync(data.OrderId);
+            if(order == null)
+            {
+                throw new InvalidOperationException($"order with id: {data.OrderId} does not found");
+            }
+            order.OrderStatusId = data.OrderStatusId;
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<Order?> GetOrderById(int id)
+        {
+            return await _db.orders.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<OrderStatus>> GetOrderStatuses()
+        {
+            return await _db.orderStatuses.ToListAsync();
+        }
+
+        public async Task TogglePaymentStatus(int orderId)
+        {
+            var order = await _db.orders.FindAsync(orderId);
+            if(order == null )
+            {
+                throw new InvalidOperationException($"order with id:{orderId} does not found");
+            }
+            order.IsPaid = !order.IsPaid;
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Order>> UserOrders(bool getAll = false)
+        {
+            var orders = _db.orders
+                            .Include(x => x.OrderStatus)
+                            .Include(x => x.OrderDetail)
+                            .ThenInclude(x => x.Book)
+                            .ThenInclude(x => x.Genre).AsQueryable();
+            if(!getAll)
+            {
+                var userId = GetUserId();
+                if (string.IsNullOrEmpty(userId))
+                    throw new Exception("User is not Logged-In");
+                orders=orders.Where(x => x.UserId == userId);
+                return await orders.ToListAsync();
+            }
+            return await orders.ToListAsync();
+        }
+
+        private string GetUserId()
+        {
+            var principal = _httpContextAccessor.HttpContext.User;
+            string userId = _userManager.GetUserId(principal);
+            return userId;
+        }
+    }
+}
